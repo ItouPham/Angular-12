d5ec8b3d7b8b98742db9efe7513141d09f5fd7d6
Cuong Pham <pvcuongkg123@gmail.com>
2023-02-15 17:08:28 +0700

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  name = 'Angular';
 
+  number = [1,3,5,7,8,10];
+  ngOnInit() {}
+
+  resetText() {
+    this.name = '';
+  }
 }
